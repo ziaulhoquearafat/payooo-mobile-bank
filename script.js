0554a18ab412b700be66320d4793a36a32582df0
Arafat <mdarafat3167@gmail.com>
2025-08-23 19:50:39 +0600

    complete login functionality

---
+++ script.js
@@ -0,0 +1,25 @@
+// login button functionality
+document.getElementById('loginButton')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+
+        const mobileNumber = 12345678910;
+        const pinNumber = 1234;
+
+        mobileNumber.value = '';
+        pinNumber.value = '';
+
+        const mobileNumberValue = document.getElementById('mobile-number').value;
+        const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+        const pinNumberValue = document.getElementById('pin-number').value;
+        const pinNumberValueConverted = parseInt(pinNumberValue);
+
+        if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+            window.location.href = './home.html';
+        }
+        else{
+            alert('Invalid Credentials');
+        }
+              
+    });
